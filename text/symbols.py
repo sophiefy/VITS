a85f1802b62263f0048c2a760e3c840af56efaae
Francis-Hu <87693204+Francis-Komizu@users.noreply.github.com>
2022-09-18 22:42:13 +0800

    Update symbols.py

--- text/symbols.py
+++ text/symbols.py
@@ -5,12 +5,13 @@ Defines the set of symbols used in text input to the model.
 '''
 _pad        = '_'
 _punctuation = ',.!?-~'
-_letters = 'AEINOQUabdefghijkmnoprstuvwyz'
+_punctuation_zh = '；：，。！？-“”《》、 '
+_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 _letters_ipa = 'ʃʧ↓↑ '
-
+_numbers = '123450'
 
 # Export all symbols:
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa) + list(_numbers) + list(_punctuation_zh)
 
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
